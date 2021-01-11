aed694effd401519443ac154d1846a6e2d1b69a8
jpnoll <jpatnoll@gmail.com>
2021-01-11 13:53:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = ["Vaccine Safety."];
+  var dataText = ["Safety"];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
